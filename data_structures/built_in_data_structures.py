09f63865f6b57dc7781c1b648a92b64c529d2747
Victor! <victor88@uw.edu>
2022-03-18 19:48:52 -0700

    work.

--- data_structures/built_in_data_structures.py
+++ data_structures/built_in_data_structures.py
@@ -16,18 +16,17 @@ another_list = ["Cat", 678, 45.09342, my_list, "name of the game"]
 #no worries if you don't have your terminal up, this is the print out
 """['Cat', 678, 45.09342, ['Bar', 'Food', 'USA'], 'name of the game']"""
 
-"""Now lets get some important information out there about lists in Python. <br/>
-- They are ordered, meaning items in the list have a defined order and that order will NOT change. If you add an item to the list, the item is placed at the end. <br/>
+"""Now lets get some important information out there about lists in Python. 
+- They are ordered, meaning items in the list have a defined order and that order will NOT change. If you add an item to the list, the item is placed at the end.
 - Lists have the ability to change! So we can add and remove items after the list has been created. 
 - Lists can have duplicate values
 - As said before, they can be of any data type. 
-
 Its because of these features that a list is mutable and dynamic! 
 
 Lets look back at the first list and see this in action. 
 """
 
-print(my_list[2])
+#print(my_list[2])
 
 #USA prints out because it is the item at the 2nd index of the ordered list. 
 #   0       1       2
@@ -38,8 +37,43 @@ print(my_list[2])
 # lets change the item at index[2] to a integer. 
 
 my_list[2] = 1776
-print(my_list[2])
+#print(my_list[2])
 
 # now lets look at that list with its new item. 
-print(my_list)
+#print(my_list)
 
+"""
+We can create lists in a number of ways 
+
+"""
+third_list = [3,4,"this", 56.0]
+fourth_list = list([56,"the","pika", 797, False])
+
+#print(third_list)
+#print(fourth_list)
+
+"""
+We can also use the range function which will return a sequence of numbers 
+"""
+#seq_of_nums = list(range(0,100))
+#print(seq_of_nums)
+
+"""
+The list() casting converts the range. 
+"""
+"""
+We can check to see how long a list is using the len function.
+"""
+#print(len(seq_of_nums)) # returns 100. 
+
+"""
+We can merge one, two, or all lists together!
+"""
+merged_lists = seq_of_nums + my_list + another_list + third_list + fourth_list
+
+#print(merged_lists)
+
+"""
+One finally, really cool thing about lists within lists is how we can access the items from a list within the original list. 
+"""
+print(merged_lists[60])
