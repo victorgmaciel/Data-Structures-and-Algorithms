d42793d987c80afa41dd1348e86f491ed7095d18
victorgmaciel <vvv.maciel@gmail.com>
2022-05-10 15:03:47 -0700

    work for arrays.

--- data_structures/array/arrays.py
+++ data_structures/array/arrays.py
@@ -1,9 +1,12 @@
 """ 
 1. Two-Sum Problem
-one approach is iterating through every single pair of integers
+A. one approach is iterating through every single pair of integers
 this is costly and results in a O(n2) because of the nested loops.
 
  """
+from unittest import result
+
+
 nums_list = [-2,1,2,4,7,11]
 target = 13;
 
@@ -18,10 +21,29 @@ def find_target(tar_num, arr):
     return False
 
 
-print(find_target(target, nums_list))
-target = 20
-print(find_target(target, nums_list))
+# print(find_target(target, nums_list))
+# target = 20
+# print(find_target(target, nums_list))
 
+""" 
+B. A better approach would be O(n)
+hash table perhaps 
+ """
 
+def two_sum_hash_table(target_num, arr):
+    result_dict = dict() #create an empty dictionary 
+    for i in range(len(arr)):
+        if arr[i] in result_dict:
+            print(result_dict[arr[i]], arr[i])
+            return True
+        else:
+            result_dict[target_num - arr[i]] = arr[i]
+    return False
+
+A = [-2, 1,2,4,7,11]
+
+target = 13
+
+print(two_sum_hash_table(target, A))
 
 
